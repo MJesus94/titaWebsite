bac8675da4f00d002ce3f9d52f0fea7e5fad252d
Miguel-Jesus <miguel.angelo.jesus@hotmail.com>
2023-05-11 18:35:27 +0100

    finger movement in the carousel added, adjustment on the carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,89 +1,3 @@
-/* import "./Carousel.css";
-
-import React from "react";
-import { useState, useRef, useEffect, useCallback } from "react";
-
-function Carousel({ slides }) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [title, setTitle] = useState(slides[0].title);
-
-  const timerRef = useRef(null);
-
-  const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-    setTitle(slides[newIndex].title);
-  };
-
-  const goToNext = useCallback(() => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-    setTitle(slides[newIndex].title);
-  }, [currentIndex, slides]);
-
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-    setTitle(slides[slideIndex].title);
-  };
-
-  const getSlideStyles = (slideIndex) => ({
-    backgroundImage: `url(${slides[slideIndex].url})`,
-  });
-
-  const getSlidesContainerWidth =() => ({
-    width: `${slides.length * 75}vw`,
-    transform: `translateX(${-(currentIndex * 75)}vw)`
-  }) 
-
-  useEffect(() => {
-    if (timerRef.current) {
-      clearTimeout(timerRef.current);
-    }
-    timerRef.current = setTimeout(() => {
-      goToNext();
-    }, 3000);
-
-    return () => clearTimeout(timerRef.current);
-  }, [goToNext]);
-
-  return (
-    <div className="slideContainer">
-      <div className="leftArrow" onClick={goToPrevious}>
-        ❰
-      </div>
-      <div className="rightArrow" onClick={goToNext}>
-        ❱
-      </div>
-      <div className="slideShowContainer">
-        {slides.map((_, slideIndex) => (
-          <div
-            className="slideShow"
-            key={slideIndex}
-            style={getSlideStyles(slideIndex)}
-          ></div>
-        ))}
-      </div>
-      <div className="dashContainer">
-        {slides.map((slide, slideIndex) => (
-          <div
-            className="dashes"
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-          >
-            ‒
-          </div>
-        ))}
-      </div>
-      <h3 className="title">{title}</h3>
-    </div>
-  );
-}
-
-export default Carousel;
- */
-
 import "./Carousel.css";
 
 import React from "react";
@@ -92,6 +6,8 @@ import { useState, useRef, useEffect, useCallback } from "react";
 function Carousel({ slides }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [title, setTitle] = useState(slides[0].title);
+  const [touchStartX, setTouchStartX] = useState(null);
+  const [touchEndX, setTouchEndX] = useState(null);
 
   const timerRef = useRef(null);
 
@@ -113,14 +29,53 @@ function Carousel({ slides }) {
     setCurrentIndex(slideIndex);
     setTitle(slides[slideIndex].title);
   };
+
+  const handleTouchStart = (event) => {
+    setTouchStartX(event.changedTouches[0].clientX);
+  };
+
+  const handleTouchMove = (event) => {
+    setTouchEndX(event.changedTouches[0].clientX);
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStartX && touchEndX) {
+      if (touchEndX < touchStartX) {
+        goToNext();
+      } else if (touchEndX > touchStartX) {
+        goToPrevious();
+      }
+    }
+
+    setTouchStartX(null);
+    setTouchEndX(null);
+  };
+
   const slideStyle = (slideIndex) => ({
     backgroundImage: `url(${slides[slideIndex].url})`,
   });
 
-  const getSlidesContainerWidth = () => ({
-    width: `${slides.length * 75}vw`,
-    transform: `translateX(${-(currentIndex * 75)}vw)`,
-  });
+  const getSlidesContainerWidth = () => {
+    let containerWidth = "";
+    let containerTransform = "";
+    const viewportWidth = window.innerWidth;
+
+    if (viewportWidth < 376) {
+      containerWidth = `${slides.length * viewportWidth}px`;
+      containerTransform = `translateX(${-(currentIndex * viewportWidth)}px)`;
+    } else if (viewportWidth >= 768 && viewportWidth < 1024) {
+      containerWidth = `${slides.length * viewportWidth}px`;
+      containerTransform = `translateX(${-(currentIndex * viewportWidth)}px)`;
+    } else {
+      containerWidth = `${slides.length * 75}vw`;
+      containerTransform = `translateX(${-(currentIndex * 75)}vw)`;
+    }
+
+    return {
+      width: containerWidth,
+      transform: containerTransform,
+    };
+  };
 
   useEffect(() => {
     if (timerRef.current) {
@@ -134,7 +89,12 @@ function Carousel({ slides }) {
   }, [goToNext]);
 
   return (
-    <div className="slideContainer">
+    <div
+      className="slideContainer"
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <div className="leftArrow" onClick={goToPrevious}>
         ❰
       </div>
