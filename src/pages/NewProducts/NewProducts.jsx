2adbc090dcd4fae650a66e173e33473315b42855
Miguel-Jesus <miguel.angelo.jesus@hotmail.com>
2023-11-23 16:44:24 +0000

    new product changes

--- src/pages/NewProducts/NewProducts.jsx
+++ src/pages/NewProducts/NewProducts.jsx
@@ -218,7 +218,7 @@ function NewProducts() {
                   Description
                 </label>
                 <textarea
-                  className="addProductInput"
+                  className="addProductInput textAreaDescription"
                   rows={4}
                   cols={40}
                   name="description"
